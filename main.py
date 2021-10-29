f27e8453210493a777c341ab4ba9ad343b7ac58e
Harshit Kr Vishwakarma <vharshitkr01gmail.com>
2021-10-29 22:20:32 +0530

    Register and Login
    1. Added register.html and login.html
    2. Added the register and login routes in main.py
    3. Updated main.py by adding Users DB
    4. Configured register route for user login

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from flask import Flask, render_template, redirect, url_for, request
+import re
+from flask import Flask, render_template, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
-from post_form import CreatePostForm
+import sqlalchemy
+from post_form import CreatePostForm, CreateLoginForm, CreateRegisterForm
 from flask_ckeditor import CKEditor
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 import datetime
 
 app = Flask(__name__)
@@ -11,12 +15,14 @@ ckeditor = CKEditor(app)
 Bootstrap(app)
 
 ##CONNECT TO DB
+app.config['SECRET_KEY'] = 'any-secret-key-you-choose'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-##CONFIGURE TABLE
+# CONFIGURE TABLE BLOG DB
 class BlogPost(db.Model):
+    __tablename__ = "blog_post"
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(250), unique=True, nullable=False)
     subtitle = db.Column(db.String(250), nullable=False)
@@ -25,7 +31,17 @@ class BlogPost(db.Model):
     author = db.Column(db.String(250), nullable=False)
     img_url = db.Column(db.String(250), nullable=False)
 
+# CONFIGURE TABLE USER DB
 
+class User(UserMixin, db.Model):
+    __tablename__ = "user_db"
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(1000)) 
+
+# db.create_all()
+# Getting all posts
 posts = db.session.query(BlogPost).all()
 
 @app.route('/')
@@ -90,6 +106,39 @@ def delete_post(id):
     db.session.delete(delete_post)
     db.session.commit()
     return redirect(url_for('home'))
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = CreateLoginForm()
+    if form.validate_on_submit():
+        email = form.email.data
+        password = form.password.data
+    return render_template("login.html", form=form)
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    form = CreateRegisterForm()
+    if form.validate_on_submit():
+        
+        new_user = User(
+            name = form.name.data,
+            email = form.email.data,
+            password = generate_password_hash(
+                form.password.data,
+                method="pbkdf2:sha256",
+                salt_length=8
+            )
+        )
+
+        try:
+            db.session.add(new_user)
+            db.session.commit()
+            return redirect(url_for("home"))
+        except sqlalchemy.exc.IntegrityError:
+            flash("You've already signed up with that email, log in instead!")
+            return redirect(url_for("login"))
+
+    return render_template("register.html", form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- post_form.py
+++ post_form.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, URL
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, URL, Email
 from flask_ckeditor import CKEditorField
 
 class CreatePostForm(FlaskForm):
@@ -11,3 +11,14 @@ class CreatePostForm(FlaskForm):
 
     body = CKEditorField("Blog Content", validators=[DataRequired()])
     submit = SubmitField("Submit Post")
+
+class CreateLoginForm(FlaskForm):
+    email = StringField("Your email", validators=[DataRequired(), Email()])
+    password = PasswordField("Your password", validators=[DataRequired()])
+    submit = SubmitField("Login")
+
+class CreateRegisterForm(FlaskForm):
+    email = StringField("Your email", validators=[DataRequired(), Email()])
+    password = PasswordField("Your password", validators=[DataRequired()])
+    name = StringField("Your name", validators=[DataRequired()])
+    submit = SubmitField("Register")
