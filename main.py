b061803ea096ff37e56eba5866c607410718a221
Harshit Kr Vishwakarma <vharshitkr01gmail.com>
2021-11-02 11:06:46 +0530

    added favicon image

--- main.py
+++ main.py
@@ -103,7 +103,7 @@ def admin_only(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         #If id is not 1 then return abort with 403 error
-        if current_user.id != 1:
+        if current_user.id > 2:
             return abort(403)
         #Otherwise continue with the route function
         return f(*args, **kwargs)        
